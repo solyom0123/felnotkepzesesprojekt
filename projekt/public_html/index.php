abb8d85417c20811ded10d0887a420c4c08972ba
user <solyom0123@freemail.hu>
2018-12-12 09:14:34 +0100

    mappa szerkezet

--- projekt/public_html/index.php
+++ projekt/public_html/index.php
@@ -1,3 +1,5 @@
+<?php
+?>
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -20,26 +22,6 @@ and open the template in the editor.
     <body class="wd-1250">
         <nav class="navbar navbar-fixed-top navbar-inverse wd-1250">
 
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#"><img width="100" src="img/logo_100x50.PNG" alt="COrvin Köz OKtatási Központ"></a>
-                <a class="navbar-brand" href="#">WebSiteName</a>
-            </div>
-
-            <div class="navbar-right pd-r-100">
-                <ul class="nav  navbar-nav ">
-
-                    <li class="dropdown  ">
-                        <a class="dropdown-toggle" data-toggle="dropdown" >Bejelentkezés
-                            <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Tanár</a></li>
-                            <li><a href="#">Diák</a></li>
-                            <li><a href="#">Admin</a></li>
-                        </ul>
-                    </li>
-
-                    </ul>
-            </div>
         </nav>
         <main class="wd-1250">
             <div class="menu ">
---
+++ projekt/public_html/php/header.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#"><img width="100" src="img/logo_100x50.PNG" alt="COrvin Köz OKtatási Központ"></a>
+                <a class="navbar-brand" href="#">WebSiteName</a>
+            </div>
+
+            <div class="navbar-right pd-r-100">
+                <ul class="nav  navbar-nav ">
+
+                    <li class="dropdown  ">
+                        <a class="dropdown-toggle" data-toggle="dropdown" >Bejelentkezés
+                            <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Tanár</a></li>
+                            <li><a href="#">Diák</a></li>
+                            <li><a href="#">Admin</a></li>
+                        </ul>
+                    </li>
+
+                    </ul>
+            </div>

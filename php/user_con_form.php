80a9975dae9116bfae2b45042d4bc66c431f2bdf
user <solyom0123@freemail.hu>
2019-08-12 20:16:28 +0200

    update: base student connect functions

--- php/user_con_form.php
+++ php/user_con_form.php
@@ -26,7 +26,7 @@
             </div> 
 
             <div class="col-md-4 ">
-                <a href="#" data-toggle="tooltip" title="Írja be a felvintendő személy nevét!"><img src="img/help.png" class="img-circle " alt="Súgó" width="15" height="15"></a>
+                <a href="#" data-toggle="tooltip" title="Képzés neve!"><img src="img/help.png" class="img-circle " alt="Súgó" width="15" height="15"></a>
             </div>                            
         </div>
         <div class="form-group row">
@@ -36,30 +36,33 @@
             </div> 
 
             <div class="col-md-4 ">
-                <a href="#" data-toggle="tooltip" title="Írja be a felvintendő személy nevét!"><img src="img/help.png" class="img-circle " alt="Súgó" width="15" height="15"></a>
+                <a href="#" data-toggle="tooltip" title="Képzés belső neve!"><img src="img/help.png" class="img-circle " alt="Súgó" width="15" height="15"></a>
             </div>                            
         </div>
         <div class="form-group row">
             <label for="form-row-student" class="col-md-4 col-form-label">Tanulók:</label>
-            <div class="col-md-2">
-                <select multiple class="form-control" id="form-row-student">
-                    <option>Diákok 1</option>
-                    <option>Diákok 2</option>
-                    <option>Diákok 3</option>
-                    <option>Diákok 4</option>
+            <div class="col-md-4">
+                <select class="form-control" id="form-row-student">
                 </select>
             </div> 
             <div class="col-md-4 ">
-                <a href="#" data-toggle="tooltip" title="Írja be a felvintendő személy nevét!"><img src="img/help.png" class="img-circle " alt="Súgó" width="15" height="15"></a>
+                <a href="#" data-toggle="tooltip" title="Válassza ki, mely diákot szeretné rendelni!"><img src="img/help.png" class="img-circle " alt="Súgó" width="15" height="15"></a>
             </div>                            
         </div>
-        
+        <div class="form-group row">
+            <label for="form-row-student" class="col-md-12 col-form-label"> Hozzárendelt tanulók:</label>
+             
+        </div>
+         <div class="form-group row">
+            <div class="col-md-12">
+                <table  id="studentTable">
+                </table>
+            </div>              
        
+        </div>
          <div class="form-group row">
         
-             <div onclick="link('actually_course')" ><input type="button" name="log-form" class="btn col-md-5 btn option-button" value="Módosítás"></div>
-        <div class="col-md-2"> </div>
-        <div onclick="megsem()"><input type="button" class="btn col-md-5 option-button" value="Mégsem"></div>
+          <div onclick="megsem()"><input type="button" class="btn col-md-12 option-button" value="Mégsem"></div>
 
 
 
